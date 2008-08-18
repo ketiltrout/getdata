71c456ea22df48afb53651e530d430a09591933c
D. V. Wiebe <getdata@ketiltrout.net>
2008-08-18 16:01:45 +0000

    People tell me this isn't the GNU C Library.  I'm skeptical, but I guess I'll defer to their jugdement.

--- bin/checkdirfile.c
+++ bin/checkdirfile.c
@@ -7,13 +7,13 @@
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * The GNU C Library is distributed in the hope that it will be useful,
+ * checkdirfile is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with the GNU C Library; if not, write to the Free
+ * License along with checkdirfile; if not, write to the Free
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA.
  */
