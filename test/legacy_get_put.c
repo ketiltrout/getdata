6c741f9500c6e14295d340c00b7b2223d50061b9
D. V. Wiebe <getdata@ketiltrout.net>
2008-09-18 16:09:27 +0000

    New legacy tests

---
+++ test/legacy_get_put.c
@@ -0,0 +1,61 @@
+/* Attempt a write following a read via the legacy API -- this requires
+ * closing and then re-opening the legacy dirfile */
+#include "../src/getdata.h"
+
+#include <inttypes.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(void)
+{
+#ifndef GETDATA_LEGACY_API
+  return 77; /* skipped */
+#else
+  const char* filedir = __TEST__ "dirfile";
+  const char* format = __TEST__ "dirfile/format";
+  const char* data = __TEST__ "dirfile/data";
+  const char* format_data = "data RAW UINT8 8\n";
+  uint8_t c[8], d;
+  unsigned char data_data[256];
+  int fd, i;
+
+  memset(c, 0, 8);
+  mkdir(filedir, 0777);
+
+  for (i = 0; i < 8; ++i)
+    c[i] = (uint8_t)(40 + i);
+
+  for (fd = 0; fd < 256; ++fd)
+    data_data[fd] = (unsigned char)fd;
+
+  fd = open(format, O_CREAT | O_EXCL | O_WRONLY, 0666);
+  write(fd, format_data, strlen(format_data));
+  close(fd);
+
+  fd = open(data, O_CREAT | O_EXCL | O_WRONLY, 0666);
+  write(fd, data_data, 256);
+  close(fd);
+
+  int get_error;
+  GetData(filedir, "data", 5, 0, 1, 0, 'c', c, &get_error);
+  int put_error;
+  int n = PutData(filedir, "data", 5, 0, 1, 0, 'c', c, &put_error);
+
+  unlink(data);
+  unlink(format);
+  rmdir(filedir);
+
+  if (get_error)
+    return 1;
+  if (put_error)
+    return 1;
+  if (n != 8)
+    return 1;
+
+  return 0;
+#endif
+}
---
+++ test/legacy_get_rofs.c
@@ -0,0 +1,57 @@
+/* Attempt to read UINT8 from a read-only filesystem via the legacy interface */
+#include "../src/getdata.h"
+
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(void)
+{
+#ifndef GETDATA_LEGACY_API
+  return 77; /* skipped */
+#else
+  const char* filedir = __TEST__ "dirfile";
+  const char* format = __TEST__ "dirfile/format";
+  const char* data = __TEST__ "dirfile/data";
+  const char* format_data = "data RAW UINT8 8\n";
+  unsigned char c[8];
+  unsigned char data_data[256];
+  int fd, i;
+
+  memset(c, 0, 8);
+  mkdir(filedir, 0777);
+
+  for (fd = 0; fd < 256; ++fd)
+    data_data[fd] = (unsigned char)fd;
+
+  fd = open(format, O_CREAT | O_EXCL | O_WRONLY, 0444);
+  write(fd, format_data, strlen(format_data));
+  close(fd);
+
+  fd = open(data, O_CREAT | O_EXCL | O_WRONLY, 0444);
+  write(fd, data_data, 256);
+  close(fd);
+
+  int error;
+  int n = GetData(filedir, "data", 5, 0, 1, 0, 'c', c, &error);
+
+  unlink(data);
+  unlink(format);
+  rmdir(filedir);
+
+  if (error)
+    return 1;
+  if (n != 8)
+    return 1;
+  for (i = 0; i < 8; ++i)
+    if (c[i] != 40 + i)
+      return 1;
+
+  return 0;
+#endif
+}
