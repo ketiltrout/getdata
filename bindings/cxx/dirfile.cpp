c978e427e9b90029021d5e580dc7427630424bae
D. V. Wiebe <getdata@ketiltrout.net>
2008-10-28 20:40:27 +0000

    Move C++ headers to subdir

--- bindings/cxx/dirfile.cpp
+++ bindings/cxx/dirfile.cpp
@@ -18,7 +18,7 @@
 // along with GetData; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
-#include "dirfile.h"
+#include "getdata/dirfile.h"
 
 using namespace GetData;
 
