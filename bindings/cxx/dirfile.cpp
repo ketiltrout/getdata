5aa27b0282f5f47749513d1a34b03b5ae44669c9
D. V. Wiebe <getdata@ketiltrout.net>
2015-10-29 01:02:28 +0000

    Fix formatting.

--- bindings/cxx/dirfile.cpp
+++ bindings/cxx/dirfile.cpp
@@ -18,7 +18,6 @@
 // along with GetData; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
-#include "getdata/dirfile.h"
 #include "internal.h"
 
 // This is not part of the Dirfile class, but it's convenient to put it here
