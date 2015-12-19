32f0732e7d8f5758f4b63fd9941cf1beb4505bb8
D. V. Wiebe <getdata@ketiltrout.net>
2015-12-19 04:01:11 +0000

    Tests.

---
+++ test/seek_range.c
@@ -0,0 +1,63 @@
+/* Copyright (C) 2015 D. V. Wiebe
+ *
+ ***************************************************************************
+ *
+ * This file is part of the GetData project.
+ *
+ * GetData is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * GetData is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GetData; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#include "test.h"
+
+int main(void)
+{
+  const char *filedir = "dirfile";
+  const char *format = "dirfile/format";
+  const char *data = "dirfile/data";
+  const char *format_data = "data RAW UINT8 2\n";
+  unsigned char data_data[256];
+  int fd, e1, r = 0;
+  off_t m;
+  DIRFILE *D;
+
+  rmdirfile();
+  mkdir(filedir, 0777);
+
+  for (fd = 0; fd < 256; ++fd)
+    data_data[fd] = (unsigned char)fd;
+
+  fd = open(format, O_CREAT | O_EXCL | O_WRONLY, 0666);
+  write(fd, format_data, strlen(format_data));
+  close(fd);
+
+  fd = open(data, O_CREAT | O_EXCL | O_WRONLY | O_BINARY, 0666);
+  write(fd, data_data, 256);
+  close(fd);
+
+  D = gd_open(filedir, GD_RDONLY);
+  /* Sample position: 2 * INT64_MAX (bad) */;
+  m = gd_seek64(D, "data", GD_INT64_MAX, 0, GD_SEEK_SET);
+  CHECKI(m, -1);
+
+  e1 = gd_error(D);
+  CHECKI(e1, GD_E_RANGE);
+
+  gd_discard(D);
+
+  unlink(data);
+  unlink(format);
+  rmdir(filedir);
+
+  return r;
+}
