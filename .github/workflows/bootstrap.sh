445897f186c9dff6e4a8655c2ed2d7b060dd1785
Sasha Rahlin <arahlin@uchicago.edu>
2024-10-17 17:39:03 -0500

    Workflow for PyPI wheels

---
+++ .github/workflows/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+here=$(cd `dirname $0`; pwd -P)
+echo $here
+cd $here/../../
+
+autoreconf -i
+./configure --disable-php \
+            --disable-fortran \
+            --disable-perl \
+            --disable-matlab \
+            --disable-idl \
+            --disable-cplusplus \
+            --with-python=`which python3` \
+            --with-pcre=$PREFIX \
+            --with-ltdl=$PREFIX \
+            --with-liblzma=$PREFIX \
+            --with-libFLAC=$PREFIX \
+            $@
